315388a3e8d824b0a589d5a71a8f7889dd3f79cc
nisha <nishprg95@gmail.com>
2026-01-09 19:09:02 +0530

    test revrse

---
+++ src/main/java/pragya/ReverseVowels.java
@@ -0,0 +1,36 @@
+package pragya;
+
+public class ReverseVowels {
+
+    public static String reverseVowels(String input) {
+        char[] chars = input.toCharArray();
+        int left = 0, right = chars.length - 1;
+        String vowels = "aeiouAEIOU";
+
+        while (left < right) {
+            while (left < right && vowels.indexOf(chars[left]) == -1) {
+                left++;
+            }
+            while (left < right && vowels.indexOf(chars[right]) == -1) {
+                right--;
+            }
+
+            if (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        String input = "testproduct";
+        String result = reverseVowels(input);
+        System.out.println("Original: " + input);
+        System.out.println("Reversed Vowels: " + result);
+    }
+}
